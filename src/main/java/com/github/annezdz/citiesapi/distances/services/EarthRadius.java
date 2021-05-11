9fa0f688cebdea697ce163238001d9ee45317cb2
annezdz <annezdz@hotmail.com>
2021-05-10 23:23:52 -0300

    Created Utils

---
+++ src/main/java/com/github/annezdz/citiesapi/distances/services/EarthRadius.java
@@ -0,0 +1,22 @@
+package com.github.annezdz.citiesapi.distances.services;
+
+public enum EarthRadius {
+    METERS("m", 6378168),
+    KILOMETERS("Km", 6378.168F),
+    MILES("mi", 3958.747716F);
+
+    private final String unit;
+    private final float value;
+
+    EarthRadius(final String unit, final float value) {
+        this.unit = unit;
+        this.value = value;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }}
